b5c15a733f7b468443c6f1a620fa74373be5afc2
Bill Conley <88862631+BillConley01@users.noreply.github.com>
2021-12-13 16:08:09 -0500

    Add files via upload

---
+++ src/app.jsx
@@ -0,0 +1,71 @@
+import React from "react"
+import ATMDeposit from "./atmDeposit";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  const [deposit, setDeposit] = React.useState(0);
+  const [totalState, setTotalState] = React.useState(0);
+  const [atmMode, setAtmMode] = React.useState("");
+  const [validTransaction, setValidTransaction] = React.useState(false);
+  // Clears input field after form is submitted or ATM mode Changed
+  const [value, setValue] = React.useState("");
+
+  let status = `Available Funds $ ${totalState} `;
+  console.log(`Account Rendered with mode: ${atmMode}`);
+  const handleChange = (event) => {
+    console.log(`handleChange ${event.target.value}`);
+    setValue(event.target.value);
+    console.log(`handleChangedvalue ${value}`);
+    setDeposit(Number(event.target.value));
+    if(event.target.value<=0){
+      setValidTransaction(false);
+    } else if(atmMode=="Cash Back" && Number(event.target.value) >totalState){
+      setValidTransaction(false);
+    } else setValidTransaction(true);
+    console.log(`valid ${validTransaction}`);
+  };
+  const handleSubmit = (event) => {
+    let newTotal = 0;
+    if(atmMode=="Cash Back" && Number(deposit) >totalState){
+      newTotal = totalState;
+      setValidTransaction(false);
+    }else if(atmMode=="Cash Back" && Number(deposit) <=totalState){
+       newTotal = totalState - deposit;
+    } else if (atmMode=="Deposit" && Number(deposit) >=0){
+      newTotal = totalState + deposit;
+    } else newTotal = totalState;
+    setTotalState(newTotal);
+    // Clears input field after form is submitted
+    setValue("");
+    event.preventDefault();
+  };
+
+  const handleModeSelect = (event) => {
+    setAtmMode(event.target.value);
+    // Clears input field after ATM mode Changed
+    setValue("");
+    //setAtmMode(event.target.value);
+    event.preventDefault();
+  };
+
+
+  return (
+    <div className="vh-100 w-100 pt-5"style={{backgroundColor: 'LightSteelBlue', marginTop:0}}>
+    <form 
+    className="mx-auto shadow-lg w-75 card text-center p-3" style={{maxWidth: 400, borderRadius: 10, backgroundColor: 'MediumTurquoise'}}
+    onSubmit={handleSubmit}>
+      <h2 className="pt-1">Neighborhood ATM</h2>
+      
+      <label className="py-2">Select an action below to continue</label>
+      <select className="form-select py-2 mb-2 w-50 mx-auto" onChange={(e) => handleModeSelect(e)} name="mode" id="mode-select">
+        <option id="no-selection" value=""></option>
+        <option id="deposit-selection" value="Deposit">Deposit</option>
+        <option id="cashback-selection" value="Cash Back">Cash Back</option>
+      </select>
+      <h3 className="py-2" id="total">{status}</h3>
+      <ATMDeposit onChange={handleChange} atmMode={atmMode} validTransaction={validTransaction} value={value}></ATMDeposit>
+    </form>
+    </div>
+  );
+};
+export default App;
---
+++ src/atmDeposit.jsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+const ATMDeposit = ({ onChange, atmMode, validTransaction, value }) => {
+    console.log(`ATM mode: ${atmMode}`);
+      if(atmMode != "")  {  return (
+            <div>
+              <input id="number-input" type="number" value={value} onChange={onChange}></input>
+              <input className="button mx-1" type="submit" value="Submit" disabled={!validTransaction} id="submit-input"></input>
+            </div>
+          );
+    } else return <></>;
+  };
+
+  export default ATMDeposit;
